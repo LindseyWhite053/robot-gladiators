df122bcdb432555cc4a8621824068c1105407229
Lindsey White <LindseyWhite053@Gmail.com>
2022-04-16 16:00:42 -0400

    player turns randomized

--- assets/js/game.js
+++ assets/js/game.js
@@ -31,9 +31,17 @@ return false;
 
 // fight function (now with parameter for enemy's object holding name, health, and attack values)
 var fight = function(enemy) {
+  
+  var isPlayerTurn = true;
+  if (Math.random()> 0.5){
+    isPlayerTurn = false;
+  }
+
   while (playerInfo.health > 0 && enemy.health > 0) {
     // ask player if they'd like to fight or run
-    if (fightOrSkip()) {
+    if (isPlayerTurn) {
+      
+      if (fightOrSkip()) {
       break;
     }
 
@@ -56,7 +64,8 @@ var fight = function(enemy) {
       break;
     } else {
       window.alert(enemy.name + ' still has ' + enemy.health + ' health left.');
-    }
+    } 
+  } else {
 
     // remove players's health by subtracting the amount set in the enemy.attack variable
     var damage = randomNumber(enemy.attack - 3, enemy.attack);
@@ -75,6 +84,8 @@ var fight = function(enemy) {
     } else {
       window.alert(playerInfo.name + ' still has ' + playerInfo.health + ' health left.');
     }
+  }
+  isPlayerTurn = !isPlayerTurn;
   }
 };
 
